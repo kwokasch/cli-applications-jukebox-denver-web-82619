b1be228a367c9979b1bba62f61a7621e74b3a68c
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-19 20:36:38 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -43,8 +43,8 @@ def exit_jukebox
 end
 
 def run(songs)
-#   puts "Please enter a command:"
-#   input = gets.strip()
+  puts "Please enter a command:"
+  input = gets.strip()
   
 #   while input != "exit"  
 #     if input == "list"
